65450dd0c60bc6c0a269c52e03d619e464ff7bab
frdvorak <frantisek.dvorak011@gmail.com>
2019-01-28 08:06:13 +0000

    filter method + setState to delete movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Movies extends Component {
     state = {
         movies: getMovies()
-    }
+    };
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies: movies });
+    };
+
     render() {
         return <table className="table">
             <thead>
@@ -17,11 +23,14 @@ class Movies extends Component {
             </thead>
             <tbody>
                 {this.state.movies.map(movie => (
-                    <tr>
+                    <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td>
+                            <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
