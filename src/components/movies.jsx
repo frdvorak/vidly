4e1b67ec43bd34836a84e556844b066b6bc3b0c3
frdvorak <frantisek.dvorak011@gmail.com>
2019-02-03 15:35:35 +0000

    like component working

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import 'bootstrap/dist/css/bootstrap.css';
+import Like from './common/like'
 
 class Movies extends Component {
     state = {
@@ -10,7 +11,14 @@ class Movies extends Component {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
         this.setState({ movies: movies });
     };
-
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = { ...movies[index] };
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
+        console.log('liked', movie);
+    };
     render() {
 
         const { length } = this.state.movies;
@@ -25,6 +33,8 @@ class Movies extends Component {
                             <th>Genre</th>
                             <th>Stock</th>
                             <th>Rate</th>
+                            <th />
+                            <th />
                         </tr>
                     </thead>
                     <tbody>
@@ -34,6 +44,12 @@ class Movies extends Component {
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <Like
+                                        liked={movie.liked}
+                                        onClick={() => this.handleLike(movie)}
+                                    />
+                                </td>
                                 <td>
                                     <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
                                 </td>
