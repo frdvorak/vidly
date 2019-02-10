4ab2f2af3609272111693a2a71af94b82f2341b8
frdvorak <frantisek.dvorak011@gmail.com>
2019-02-10 10:42:22 +0000

    Generating unique keys for moviesTable component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -3,8 +3,6 @@ import TableHeader from './common/tableHeader';
 import Like from './common/like';
 import TableBody from './common/tableBody';
 
-const x = <Like></Like>; //
-
 class MoviesTable extends Component {
     columns = [
         { path: 'title', label: 'Title' },
@@ -15,7 +13,7 @@ class MoviesTable extends Component {
         { key: 'delete', content: movie => <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">Delete</button> },
     ]
     render() {
-        const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
+        const { movies, sortColumn, onSort } = this.props;
         return (
             <table className="table">
                 <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort} />
