dfc298e242f55a18c7b49bdd317db50944d7bf50
frdvorak <frantisek.dvorak011@gmail.com>
2019-02-18 09:34:05 +0000

    4 routes implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <main className="container">
-          <Movies />
-        </main>
-      </div>
+      <main className="container">
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+      </main>
     );
   }
 }
